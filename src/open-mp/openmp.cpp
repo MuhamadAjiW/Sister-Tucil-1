3e3a39427c29a824ca7268c039844b4cc16f8f67
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-04-01 22:39:23 +0700

    feat: finished openmp
    Co-authored-by: Fakhrimm <13521045@mahasiswa.itb.ac.id>
    Co-authored-by: razzanYoni <13521087@mahasiswa.itb.ac.id>

--- src/open-mp/openmp.cpp
+++ src/open-mp/openmp.cpp
@@ -3,14 +3,84 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+// Macros so the overhead is in the compiler
+#define THREAD_COUNT 8
+
+void print_matrix(double** mat, int x, int y, int x_offset){
+    cout << y << endl;
+    for(int i=0; i < y; ++i)
+    {
+        for(int j = x_offset; j < x; ++j)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(void) {
-    #pragma omp parallel num_threads(8) 
-    {
-        int nthreads, tid;
-        nthreads = omp_get_num_threads();
-        tid = omp_get_thread_num();
-        printf("Hello world from from thread %d out of %d threads\n", tid, nthreads);
+    // Read matrix information in main thread
+    int n;
+
+    cin >> n;
+    int n_double = 2 * n;
+
+    // Why even initialize it with 2n x 2n in the serial code? That's not very memory efficient
+    double** mat = new double*[n];
+    for (int row = 0; row < n; ++row) {
+        mat[row] = new double[n_double]();
+        for (int col = 0; col < n; ++col){
+            cin >> mat[row][col];
+        }
     }
+
+    // Timer starts at identity matrix initialization
+    auto start = chrono::high_resolution_clock::now();
+
+    // Identity matrix
+    #pragma omp simd
+    for (int row = 0; row < n; ++row){
+        mat[row][row + n] = 1;
+    }
+
+    for (int row = 0; row < n; ++row){
+        double scale = mat[row][row];
+
+        // Single row
+        #pragma omp simd
+        for (int col  = 0; col < n_double; ++col){
+            mat[row][col] /= scale;
+        }
+        
+        // Propagate line to other rows 
+        #pragma omp parallel for num_threads(THREAD_COUNT)
+        for (int row2 = 0; row2 < n; ++row2){
+            if(row2 == row) continue;
+
+            double multiplier = mat[row2][row];
+
+            #pragma omp simd
+            for (int col = 0; col < n_double; col++){
+                mat[row2][col] -= mat[row][col] * multiplier;
+            }
+        }
+    }
+    
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> time_taken = end - start;
+    cout << time_taken.count() << " Seconds" << std::endl;
+
+    print_matrix(mat, n_double, n, n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        delete[] mat[i];
+    }
+    delete[] mat;
+
     return 0;
 }
